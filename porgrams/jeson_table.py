4f64693e54dfc33b499f347f3199e79063517e8b
Jakub Jakubiak <34916730+JakubJakubiak@users.noreply.github.com>
2022-12-21 20:39:44 +0100

    dynamic foldrs

--- porgrams/jeson_table.py
+++ porgrams/jeson_table.py
@@ -4,7 +4,7 @@ from PIL import Image, ImageEnhance, ImageFilter
 import os
 import random
 
-path = 'G:/git/pyton/notes/png'
+path = './notes/png'
 
 index = 0
 randoms = 0
@@ -12,34 +12,25 @@ randoms = 0
 filenames = []
 results = []
 
-elements = os.listdir(path)
-Foldet_elements = len(elements)
 
-for file in elements:
-        filename = os.path.splitext(file)[0]
-        filenames.append(filename)
+for file in os.listdir(path):
+    filename = os.path.splitext(file)[0]
+    filenames.append(filename)
 
 for filename in os.listdir(path):
-   
-
-    # print(filenames)
-   
-    randoms=random.randint(0, 3)
-    random_elements =  random.sample(filenames, 4) 
+    randoms = random.randint(0, 3)
+    random_elements = random.sample(filenames, 4)
     print(random_elements)
-    
 
     result = [{
         "id": index,
-        'question': 'lib/logoPng/' + random_elements[randoms]+ '.png',
+        'question': 'lib/logoPng/' + random_elements[randoms] + '.png',
         'options':  random_elements,
         'answer_index': randoms,
     }]
-            
 
     results += result
-    index = index +1
+    index = index + 1
 
     with open('result.json', 'w') as f:
         json.dump(results, f)
-      
