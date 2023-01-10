54d28839477f7c28bc8eb83e42844518fa030693
Jakub Jakubiak <34916730+JakubJakubiak@users.noreply.github.com>
2023-01-10 16:21:44 +0100

    download_image async

--- Pyton/imageDonolods.py
+++ Pyton/imageDonolods.py
@@ -1,28 +1,40 @@
-import os
-import requests
 import asyncio
+import os
+import aiohttp
+
+async def download_image(url, file_name):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                with open(os.path.join("png", file_name), "wb") as f:
+
+                    image_data = await response.read()
+                    f.write(image_data)
+                    print(f"Image {file_name} was downloaded and saved in the images folder.")
+            else:
+                print(f"Failed to download image from address {url}.")
 
 async def main():
-        
-    image_urls = [
-    'https://cdn.midjourney.com/49ce4e9b-c8e6-406f-a568-5bbaba88ba2e/grid_0_640_N.webp',
-    'https://cdn.midjourney.com/c1d742e4-283e-438e-b552-11fe12eca2a6/grid_0_640_N.webp',
-    'https://cdn.midjourney.com/9dc21fb9-9415-4e31-a1ef-149d3ee98a07/grid_0_640_N.webp',
-    ]
 
+    image_urls = [
+  'https://cdn.midjourney.com/a1a79d61-2ebd-4b04-a269-a5ab4614f0db/grid_0_640_N.webp',
+  'https://cdn.midjourney.com/c644780e-759c-44de-b989-8eb38c052c98/grid_0_640_N.webp',
+  'https://cdn.midjourney.com/2b41197d-894f-4429-b0ad-60ed0ebcdcbd/grid_0_640_N.webp',
+    ]
+    
     os.makedirs("png", exist_ok=True)
+    tasks = []
     for i, url in enumerate(image_urls):
-        file_name = f"image_{i+1}_{url.split('/')[-1]}"
+        file_name = f"image_{i+1}_{url.split('/')[-1]}"   
         
         if os.path.exists(os.path.join("png", file_name)):
-            file_name = file_name.split(".")[0] + "_new." + file_name.split(".")[1]
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            with open(os.path.join("png", file_name), "wb") as f:
-                f.write(response.content)
-                print(f"image {file_name} save")
+            print(f"Obraz {file_name} już istnieje.")
         else:
-            print(f"No Save {url}.")
+            task = asyncio.ensure_future(download_image(url, file_name))
+            tasks.append(task)
+    await asyncio.gather(*tasks)
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
